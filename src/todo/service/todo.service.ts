0527d37943165370e27cff8ba0a88544dcf656b4
eden0514 <ghworlas@naver.com>
2023-06-01 00:07:17 +0900

    All views_17:40

--- src/todo/service/todo.service.ts
+++ src/todo/service/todo.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import {PrismaService} from '../../prisma.service';
+import {Todo} from '@prisma/client';
+
 @Injectable()
 export class TodoService {
   constructor(private prismaService : PrismaService){}
-  
-  
+  //All Views
+  async fetchAllTodos() : Promise<Todo[]> {
+    return this.prismaService.todo.findMany();
+  }
 }
