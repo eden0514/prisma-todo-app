a94756bcbe4323a6a9273e952ad23a066ce3725b
eden0514 <ghworlas@naver.com>
2023-06-05 21:31:23 +0900

    create CRUD TodoService

--- src/todo/controller/todo.controller.ts
+++ src/todo/controller/todo.controller.ts
@@ -1,4 +1,39 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Delete, Post, Body,Put } from '@nestjs/common';
+import { TodoService } from '../service/todo.service';
+import { Todo } from '@prisma/client';
 
-@Controller('todo')
-export class TodoController {}
+@Controller('api/v1/todos')
+export class TodoController {
+
+  constructor(private readonly todoService: TodoService){}
+
+  @Get()
+  async fetchAllTodos(): Promise<Todo[]> {
+    return this.todoService.fetchAllTodos();
+  }
+  @Get(':id')
+  async fetchTodoItem(@Param('id') id:number): Promise<Todo | null> {
+    return this.todoService.fetchTodoItem(id);
+  }
+
+  @Delete(':id')
+  async deleteTodoItem(@Param('id') id:number): Promise<Todo | null> {
+    return this.todoService.deleteTodoItem(id);
+  }
+
+  @Post()
+  async addTodoItem(@Body() data: Todo): Promise<Todo> {
+    return this.todoService.addTodoItem(data);
+  }
+  @Put(':id')
+  async updateTodoItem(
+    @Param('id') id:number,
+    @Body() data: Todo
+    ): Promise<Todo | null> {
+    return this.todoService.updateTodoItem(
+      id,
+      data.title,
+      data.content,
+      data.is_done);
+  }
+}
--- src/todo/service/todo.service.ts
+++ src/todo/service/todo.service.ts
@@ -9,4 +9,33 @@ export class TodoService {
   async fetchAllTodos() : Promise<Todo[]> {
     return this.prismaService.todo.findMany();
   }
+  //Single View
+  async fetchTodoItem(id: number) : Promise<Todo | null> {
+    return this.prismaService.todo.findUnique({where: {id:Number(id)}});
+  }
+
+  //Delete
+  async deleteTodoItem(id: number) : Promise<Todo | null> {
+    return this.prismaService.todo.delete({where: {id:Number(id)}});
+  }
+    //Create
+    async addTodoItem(data: Todo) : Promise<Todo> {
+      return this.prismaService.todo.create({data: data});
+    }
+  //update
+  async updateTodoItem(
+    id: number,
+    title: string,
+    content:string,
+    is_done:boolean
+    ) : Promise<Todo | null> {
+    return this.prismaService.todo.update({
+      where: {id:Number(id)},
+      data: {
+        title: title,
+        content: content,
+        is_done: is_done
+      }
+    });
+  }
 }
